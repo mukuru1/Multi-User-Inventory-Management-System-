905cf780ca4af1727472a39b5bf2f537936178d7
mukuru1 <kabengaedwin@gmail.com>
2025-12-14 10:24:29 +0200

    commit

--- script.js
+++ script.js
@@ -102,6 +102,16 @@ function initRegister() {
 		});
 	}
 
+    const loginForm = document.getElementById('loginForm');
+
+function showAlert(message, type = 'danger') {
+    alertBox.textContent = message;
+    alertBox.className = `alert alert-${type} show`;
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 5000);
+}
+
     function showError(elementId, message) {
     const errorElement = document.getElementById(elementId);
     const inputElement = document.getElementById(elementId.replace('Error', ''));
@@ -109,3 +119,4 @@ function initRegister() {
     errorElement.classList.add('show');
     inputElement.classList.add('error');
 }
+
